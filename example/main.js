c026f9971ca6d253f73192f6ee1e8d29d76be6b1
kingnan <373734199@qq.com>
2023-04-24 00:24:24 +0800

    初始话 mini-webpack * 1、获取内容	createAsset * 2、获取对应的依赖关系	createGraph * 3、 创建 图 createGraph

---
+++ example/main.js
@@ -0,0 +1,5 @@
+// import { request } from "http";
+import foo from "./foo.js";
+// const  a = request("./foo")
+foo()
+console.log("--main--");
---
+++ index.js
@@ -0,0 +1,60 @@
+import fs from "fs";
+import parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import { log } from "console";
+import path from "path";
+/**
+ * 
+ * 1、获取内容
+ * 2、获取对应的依赖关系
+ *  ast -> 获取内容
+ */
+function createAsset(filePath) {
+    //  1、获取内容
+    const source = fs.readFileSync(filePath, {
+      encoding: "utf-8"
+    })
+    // 2、获取对应的依赖关系
+    const ast = parser.parse(source, {
+      sourceType: "module"
+    })
+    // console.log(ast);
+    // 存储依赖关系 dep
+    const deps = []
+    traverse.default(ast, {
+      // 当调用到 ImportDeclaration  节点的时候 会调用到 ImportDeclaration()函数
+      ImportDeclaration({ node }) {
+        log("ImportDeclaration--------\n", node.source.value);
+        deps.push(node.source.value)
+      }
+    });
+
+    return {
+      filePath,
+      source,
+      deps
+    }
+}
+
+// const asset = createAsset()
+// console.log(asset);
+
+/**
+ * 获取 图
+ */
+function createGraph() {
+  const mainAsset = createAsset("./example/main.js")
+  // 基于依赖关系 找到下一个模块
+  const queue = [mainAsset]
+  for (const asset of queue) {
+    asset.deps.forEach((relativePath) => {
+      // example/foo.js
+      const child = createAsset(path.resolve("./example", relativePath))
+      // log(child)
+      queue.push(child)
+    });
+  }
+  return queue
+}
+const graph = createGraph()
+log(graph)
